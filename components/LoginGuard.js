3721a1448e18a37378fd733fa38a48d97087d0e9
Tim <blackpr.tim@gmail.com>
2020-09-26 19:27:14 +0300

    add login guard

---
+++ components/LoginGuard.js
@@ -0,0 +1,54 @@
+import { useUser } from 'context/userContext'
+import { Text } from 'grommet'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export default function LoginGuard({ children }) {
+  const { user } = useUser()
+  const router = useRouter()
+  const [loading, setLoading] = useState(true)
+  const [isCorrectUser, setIsCorrectUser] = useState(false)
+
+  if (!user?.username) {
+    if (typeof window === 'object') {
+      router.replace('/logout')
+    }
+  }
+
+  useEffect(() => {
+    let url = `${user.host}:${user.port}/player_api.php?username=${user.username}&password=${user.password}&output=ts`
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          res
+            .json()
+            .then((data) => {
+              if (data?.user_info?.auth === 1) {
+                setIsCorrectUser(true)
+                setLoading(false)
+              }
+            })
+            .catch((e) => {
+              console.log(e)
+              setLoading(false)
+            })
+        } else {
+          setLoading(false)
+        }
+      })
+      .catch((e) => {
+        console.log(e)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <Text>loading...</Text>
+  }
+
+  if (!isCorrectUser) {
+    router.push('/logout')
+  }
+
+  return <>{children}</>
+}
