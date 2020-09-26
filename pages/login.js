2bc32f39b8bad6d67429d637e541f0f10ea1ab92
Tim <blackpr.tim@gmail.com>
2020-09-26 17:41:06 +0300

    scaffold login

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import { Anchor } from 'grommet'
+import { Anchor, Box, Button, Form, FormField, TextInput } from 'grommet'
 import LoginLayout from 'layout/Login'
 import Link from 'next/link'
 
@@ -11,5 +11,33 @@ const LoginNav = () => {
 }
 
 export default function Login() {
-  return <LoginLayout LoginNav={<LoginNav />}>Content</LoginLayout>
+  function handleSubmit({ value }) {
+    console.log('todo', value)
+  }
+  return (
+    <LoginLayout LoginNav={<LoginNav />}>
+      <Form onSubmit={handleSubmit}>
+        <FormField name="Host" htmlfor="host">
+          <TextInput
+            id="host"
+            name="Host"
+            placeholder="Host. eg: http://zzz.xxx"
+          />
+        </FormField>
+        <FormField name="Port" htmlfor="Port">
+          <TextInput id="Port" name="Port" placeholder="Port. ex: 80" />
+        </FormField>
+        <FormField name="Username" htmlfor="Username">
+          <TextInput id="Username" name="Username" placeholder="Username" />
+        </FormField>
+        <FormField name="Password" htmlfor="Password">
+          <TextInput id="Password" name="Password" placeholder="Password" />
+        </FormField>
+        <Box direction="row" gap="medium">
+          <Button type="submit" primary label="Go!" />
+          {/* <Button type="reset" label="Reset" /> */}
+        </Box>
+      </Form>
+    </LoginLayout>
+  )
 }
