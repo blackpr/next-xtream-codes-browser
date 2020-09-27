4f2e35d0d6b046d6ec1588864e5c65e71d288d7d
Tim <blackpr.tim@gmail.com>
2020-09-27 17:36:18 +0300

    sereis categories add links

--- components/SeriesCategoriesList.js
+++ components/SeriesCategoriesList.js
@@ -1,17 +1,26 @@
 import { Box, InfiniteScroll, Text } from 'grommet'
+import Link from 'next/link'
+
 export default function SeriesCategoriesList({ categories, ...props }) {
   return (
     <Box>
       <InfiniteScroll items={categories} {...props}>
         {(item) => (
-          <Box
+          <Link
+            href="/series/[categoryId]"
+            as={`/series/${item?.category_id}`}
             key={item?.category_id}
-            pad="medium"
-            border={{ side: 'bottom' }}
-            align="center"
           >
-            <Text>{item?.category_name}</Text>
-          </Box>
+            <Box
+              as="a"
+              key={item?.category_id}
+              pad="medium"
+              border={{ side: 'bottom' }}
+              align="center"
+            >
+              <Text>{item?.category_name}</Text>
+            </Box>
+          </Link>
         )}
       </InfiniteScroll>
     </Box>
---
+++ pages/series/[categoryId]/index.js
@@ -0,0 +1,3 @@
+export default function SeriesCategory() {
+  return <div>series category</div>
+}
