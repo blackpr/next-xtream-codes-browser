7cbfa7d6ea6925f03578a1c0c4ca8528b7544e69
Tim <blackpr.tim@gmail.com>
2020-09-27 15:38:17 +0300

    add home content

--- components/HomeContent.js
+++ components/HomeContent.js
@@ -1,13 +1,67 @@
-import { Box, Card, Grid, ResponsiveContext, Text } from 'grommet'
-import { useContext } from 'react'
+import { Box, Text } from 'grommet'
+import { Channel, Ticket, Video } from 'grommet-icons'
+import Link from 'next/link'
 
 export default function HomeContent() {
-  const size = useContext(ResponsiveContext)
-  const cards = Array(20)
-    .fill()
-    .map((_, i) => <Text key={i}>{`Card ${i}`}</Text>)
   return (
-    <Box pad="large">
+    <Box
+      direction="row-responsive"
+      justify="center"
+      align="center"
+      pad="large"
+      gap="medium"
+    >
+      <Link passHref href="/series">
+        <Box
+          as="a"
+          href="/series"
+          pad="large"
+          align="center"
+          background={{ color: 'brand', opacity: 'strong' }}
+          round
+          gap="small"
+        >
+          <Video size="large" />
+          <Text>Series</Text>
+        </Box>
+      </Link>
+      <Link passHref href="/movies">
+        <Box
+          as="a"
+          href="/movies"
+          pad="large"
+          align="center"
+          background={{ color: 'brand', opacity: 'strong' }}
+          round
+          gap="small"
+        >
+          <Ticket size="large" />
+          <Text>Movies</Text>
+        </Box>
+      </Link>
+      <Link passHref href="/tv">
+        <Box
+          as="a"
+          href="/tv"
+          pad="large"
+          align="center"
+          background={{ color: 'brand', opacity: 'strong' }}
+          round
+          gap="small"
+        >
+          <Channel size="large" />
+          <Text>TV</Text>
+        </Box>
+      </Link>
+    </Box>
+  )
+}
+
+// const cards = Array(20)
+// .fill()
+// .map((_, i) => <Text key={i}>{`Card ${i}`}</Text>)
+{
+  /* <Box pad="large">
       <Grid columns={size !== 'small' ? 'small' : '100%'} gap="small">
         {cards.map((card, index) => (
           <Card pad="large" key={index}>
@@ -15,6 +69,5 @@ export default function HomeContent() {
           </Card>
         ))}
       </Grid>
-    </Box>
-  )
+    </Box> */
 }
