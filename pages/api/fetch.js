eff193dbf406dc5f4c4bf49eadb61443d34b04af
Tim Pap <blackpr.tim@gmail.com>
2020-11-24 18:16:02 +0200

    try axios

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,17 +1,28 @@
+const axios = require('axios').default
+
 export default async (req, res) => {
   const url = req.query.url
   console.log('start', { url })
   try {
-    const response = await fetch(decodeURIComponent(url))
-    console.log('after response', response)
-    if (response.ok) {
-      const data = await response.json()
-      return res.json(data)
-    } else {
-      return res.status(500).json({})
-    }
+    const response = await axios.get(decodeURIComponent(url))
+    return res.json(response.data)
   } catch (e) {
     console.log(e)
   }
   return res.status(500).json({})
 }
+
+// export default async (req, res) => {
+//   const url = req.query.url
+
+//   try {
+//     const response = await fetch(url)
+//     if (response.ok) {
+//       const data = await response.json()
+//       return res.json(data)
+//     }
+//   } catch (e) {
+//     console.log(e)
+//   }
+//   res.json({})
+// }
