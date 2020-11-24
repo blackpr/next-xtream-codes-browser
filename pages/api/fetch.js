859a1ffa347a468dee687387d67c1a08811f63e0
Tim Pap <blackpr.tim@gmail.com>
2020-11-24 17:56:10 +0200

    debug

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,7 +1,9 @@
 export default async (req, res) => {
   const url = req.query.url
+  console.log('start', { url })
   try {
     const response = await fetch(decodeURIComponent(url))
+    console.log('after response', response)
     if (response.ok) {
       const data = await response.json()
       return res.json(data)
